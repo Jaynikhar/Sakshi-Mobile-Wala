b8cf2947c352a1cac555c6ecd22cbc3def6e1269
Jaynikhar <jaynikhar61@gmail.com>
2026-06-01 22:06:58 +0530

    add backend live link

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // 🔁 change if needed
+  // baseURL: "http://localhost:5000/api", // 🔁 change if needed
+  baseURL: "https://sakshi-mobile-wala.onrender.com", 
 });
 
 // // attach token automatically
